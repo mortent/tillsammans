9629e559f9b1c6b7b49b2ebf17d48e58a0a516d5
Mikael Vik <mikael.vik@gmail.com>
2008-03-18 22:14:58 +0100

    Added code for showing the map at localhost:3000/map.

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,8 +1,15 @@
 class MapController < ApplicationController
 
+  #require 'ym4r'
+  #include Ym4r::GoogleMaps
+  
   def index
+    @map = GMap.new("bekkmap")
+    @map.control_init(:large_map => true, :map_type => true)
+    @map.center_zoom_init([59.92,10.749],12)    
+    @map.overlay_init(GMarker.new([59.901,10.735], :title => "Skuret Hardkoda", :info_window => "Hardkoda info om skuret!"))
   end
-
+ 
   def event
   end
 
