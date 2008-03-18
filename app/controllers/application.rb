a31b5251185496cc1f570f47ee1f4dc99f85d0f6
oc <oc@rynning.no>
2008-03-18 20:23:01 +0100

    Inited bare Ruby on Rails project with RSpec and Spec::Rails in vendor.
    Signed-off-by: oc <oc@rynning.no>

---
+++ app/controllers/application.rb
@@ -0,0 +1,10 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => 'fd657d308d3f3fabdb0be37587c173c6'
+end
