ea28d173140a268d6c314581811dff7ccd3c0a0b
Mikael Vik <mikael.vik@gmail.com>
2008-03-18 22:30:11 +0100

    Added tillsammans layout.

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,15 +1,22 @@
 class MapController < ApplicationController
+  before_filter :setup
   
   def index    
+  end
+ 
+  def event
+    @events = Events.find(:all)
+  end
+
+  def user
+  end
+  
+  private
+  
+  def setup
     @map = GMap.new("bekkmap")
     @map.control_init(:large_map => true, :map_type => true)
     @map.center_zoom_init([59.92,10.749],12)    
     @map.overlay_init(GMarker.new([59.901,10.735], :title => "Skuret Hardkoda", :info_window => "Hardkoda info om skuret!"))
-  end
- 
-  def event
-  end
-
-  def user
   end
 end
