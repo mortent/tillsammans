7807ff9ee2938387f661b25a44f460d930a79e47
oc <oc@rynning.no>
2008-03-24 19:41:18 +0100

    Mekket et mock-design

--- app/models/map.rb
+++ app/models/map.rb
@@ -1,9 +1,10 @@
 class Map < ActiveRecord::Base
-  @@default_name = "bekkmap"
+  @@default_name = "map"
   
   def to_gmap
     gm = GMap.new(@@default_name)
-    gm.control_init(:large_map => true, :map_type => true)
+    gm.control_init(:small_map => true, :map_type => true, :overview_map => true, :scale => true)
+    gm.set_map_type_init(GMapType::G_HYBRID_MAP)
     gm.center_zoom_init([lat, lng], zoom)
     gm
   end
