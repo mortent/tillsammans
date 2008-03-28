a2d637968f6ca355ebcc7526b647cd8dfb91a4c7
oc <oc@rynning.no>
2008-03-28 09:00:06 -0600

    Added views to public controller, changed name_prefix for admin routes

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,23 +1,28 @@
-class PublicController < ApplicationController  
+class PublicController < ApplicationController
+  
+  # Show all events in map
   def index
-    @map = Map.find_by_name('Oslo')
-    @gmap = @map.to_gmap
+    @gmap = Map.find_by_name('Oslo').to_gmap
+    Event.find(:all).each { |e| @gmap.overlay_init(e.to_gmarker) }
     
-    #locations = Location.find(:all)
-    
-    #locations.each do |l| 
-    #	@gmap.overlay_init(l.to_gmarker)
-    #end
-    
-    events = Event.find(:all)
-    
-    events.each do |e|
-      @gmap.overlay_init(e.to_gmarker)
-    end
-    
-    clients = Client.find(:all)
-    clients.each do |c|
-      @gmap.overlay_init(c.to_gmarker)
-    end
   end
+  
+  # Show all clients' office-locations in map
+  def clients
+    @gmap = Map.find_by_name('Stor-Oslo').to_gmap
+    Client.find(:all).each { |c| @gmap.overlay_init(c.to_gmarker) }    
+  end
+  
+  # Show all users' locations in map
+  def users
+    @gmap = Map.find_by_name('Stor-Oslo').to_gmap
+    Users.find(:all).each { |u| @gmap.overlay_init(u.to_gmarker) }
+  end
+  
+  # Show event and all users attending in the map, sorted by their location
+  def event
+    @gmap = Map.find_by_name('Oslo').to_gmap
+    Event.find(params[:id], :include => ['users', 'clients']).each { |e| @gmap.overlay_init(e.to_gmarker) }    
+  end
+    
 end
--- app/models/map.rb
+++ app/models/map.rb
@@ -3,8 +3,8 @@ class Map < ActiveRecord::Base
   @@default_opts = { :small_map => true, :map_type => true, :overview_map => true, :scale => true }
   
   def to_gmap(opts={})  
-    opts.merge!(@@default_opts)
-    
+    opts.merge!(@@default_opts) 
+        
     gm = GMap.new(@@default_name)
     gm.control_init(opts)
     gm.set_map_type_init(GMapType::G_HYBRID_MAP)
