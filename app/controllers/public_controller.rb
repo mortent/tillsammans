3864ec842301bbc6b40f3e7c66dd845344d60b27
oc <oc@rynning.no>
2008-05-08 20:29:05 +0200

    Added spec for public index

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -2,26 +2,29 @@ class PublicController < ApplicationController
   
   # Show all events in map
   def index
-    @gmap = Map.find_by_name('Oslo').to_gmap
-    Event.find(:all).each { |e| @gmap.overlay_init(e.to_gmarker) }
-    
+    @map = Map.find_by_name('Oslo')
+    @gmap = @map.to_gmap
+    Event.find(:all).each { |e| @gmap.overlay_init(e.to_gmarker) }    
   end
   
   # Show all clients' office-locations in map
   def clients
-    @gmap = Map.find_by_name('Stor-Oslo').to_gmap
+    @map = Map.find_by_name('Stor-Oslo')    
+    @gmap = @map.to_gmap
     Client.find(:all).each { |c| @gmap.overlay_init(c.to_gmarker) }    
   end
   
   # Show all users' locations in map
   def users
-    @gmap = Map.find_by_name('Stor-Oslo').to_gmap
+    @map = Map.find_by_name('Stor-Oslo')    
+    @gmap = @map.to_gmap
     Users.find(:all).each { |u| @gmap.overlay_init(u.to_gmarker) }
   end
   
   # Show event and all users attending in the map, sorted by their location
   def event
-    @gmap = Map.find_by_name('Oslo').to_gmap
+    @map = Map.find_by_name('Oslo')
+    @gmap = @map.to_gmap
     Event.find(params[:id], :include => ['users', 'clients']).each { |e| @gmap.overlay_init(e.to_gmarker) }    
   end
     
--- spec/views/public/index.html.erb_spec.rb
+++ spec/views/public/index.html.erb_spec.rb
@@ -1,12 +1,12 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
-#describe "/public/index" do
-#  before(:each) do
-#    render 'public/index'
-#  end
-#  
-#  #Delete this example and add some real ones or delete this file
-#  it "should show a google map" do
-#    response.should have_tag('div')
-#  end
-#end
+describe "/public/index" do
+  it "should show a div with id map (google map)" do
+    map = mock_model(Map)
+    gmap = map.stub!(:to_gmap)
+    gmap.should_receive(:to_html).and_return("gmap!")  
+    assigns[:gmap] = gmap
+    render "/public/index"    
+    response.should have_tag('div#map')
+  end
+end
