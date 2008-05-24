4defa5849da248589e6cdec465c9248ac6f76748
oc <oc@rynning.no>
2008-05-24 18:16:55 +0200

    Added proper javascript in partial

---
+++ public/javascripts/flex_global.js
@@ -0,0 +1,11 @@
+<!--
+// -----------------------------------------------------------------------------
+// Globals
+// Major version of Flash required
+var requiredMajorVersion = ${version_major};
+// Minor version of Flash required
+var requiredMinorVersion = ${version_minor};
+// Minor version of Flash required
+var requiredRevision = ${version_revision};
+// -----------------------------------------------------------------------------
+// -->
